9fdcc49f3a0817f52bae6c1718166bb76d948bd1
Irina <shishokina.irina@mail.ru>
2020-09-10 12:22:22 +0700

    add project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//'DOMContentLoaded' - скрипт выполняется после полной загрузки структуры документа
+window.addEventListener('DOMContentLoaded', function () {
+	'use strict';  //объявление строгого режима
+	let tab = document.querySelectorAll('.info-header-tab'),
+		info = document.querySelector('.info-header'),
+		tabContent = document.querySelectorAll('.info-tabcontent');
+});
